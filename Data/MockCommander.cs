e59773a7a4a5978404a1d1baea71b5889da99dcc
Ayrton Vitor <ayrtonvitor@pm.me>
2024-10-14 21:13:16 -0300

    Implement mock data repo

---
+++ Data/MockCommander.cs
@@ -0,0 +1,22 @@
+using Commander.Models;
+
+namespace Commander.Data;
+
+public class MockCommander : ICommanderRepo
+{
+    public IEnumerable<Command> GetAppCommands()
+    {
+        var commands = new List<Command>
+        {
+            new() { Id = 0, HowTo = "Read a book", Line = "Read the words", Platform = "Chair" },
+            new() { Id = 1, HowTo = "Hi fi a croc", Line = "Don't", Platform = "Swap" },
+            new() { Id = 2, HowTo = "Hugh a gorilla", Line = "Just don't get up", Platform = "Jungle" }
+        };
+        return commands;
+    }
+
+    public Command GetById(int id)
+    {
+        return new Command { Id = 0, HowTo = "Read a book", Line = "Read the words", Platform = "Chair" };
+    }
+}
