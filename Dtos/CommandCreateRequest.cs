b1555fd9529898dae483a2fd6914a1700290152f
Ayrton Vitor <ayrtonvitor@pm.me>
2024-10-25 01:20:08 -0300

    Add data reqirements to create command dto
    Explicitly mark fields as required so as to return bad request when
    user does no send one of the fields.
    Put size limit to input.

--- Dtos/CommandCreateRequest.cs
+++ Dtos/CommandCreateRequest.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos;
 
 public sealed record CommandCreateRequest
 {
+    [Required]
+    [MaxLength(255)]
     public string HowTo { get; set; }
+
+    [Required]
+    [MaxLength(255)]
     public string Line { get; set; }
+
+    [Required]
+    [MaxLength(255)]
     public string Platform { get; set; }
 }
